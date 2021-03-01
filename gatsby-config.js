4525d2fc04a95f36f960d436808e89be63f5d328
Baptiste Lecocq <baptiste.lecocq@gmail.com>
2021-03-01 11:33:44 +0100

    chore: add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,18 @@ module.exports = {
     "gatsby-plugin-sass",
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "1 à 10",
+        short_name: "1 à 10",
+        start_url: "/",
+        background_color: "#ff1351",
+        theme_color: "#ff1351",
+        display: "minimal-ui",
+        icon: "src/images/icon.png",
+      },
+    },
     "gatsby-plugin-sitemap",
     "gatsby-transformer-sharp",
     {
