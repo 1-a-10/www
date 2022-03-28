e3f654aab30ecaf310fbaf195ade2e0eb50666a8
tiste <tiste@users.noreply.github.com>
2022-03-28 13:03:12 +0000

    deploy: 70599755c943f96b9815046422ff51b9683d632f

---
+++ component---src-pages-404-js-bfceaeaddcea0a793d2a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwww=self.webpackChunkwww||[]).push([[883],{429:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var l=n(7294),u=n(5454);function r(){return l.createElement(u.A,{title:"404"},l.createElement("div",{className:"not-found"},l.createElement("h1",null,"404"),l.createElement("h2",null,"Document not found"),l.createElement("p",null,l.createElement("a",{href:"/"},"Return to homepage"))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-bfceaeaddcea0a793d2a.js.map
