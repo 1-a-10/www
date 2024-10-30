053a2b50625478a34706caac276c3d6e0c9a79f8
Baptiste Lecocq <baptiste.lecocq@gmail.com>
2024-10-30 16:40:42 +0100

    feat: add google forms

--- src/pages/contribute.js
+++ src/pages/contribute.js
@@ -9,16 +9,17 @@ export default function ContributePage() {
       <section className="section">
         <div className="container content">
           <h1 className="title is-1">Je veux contribuer à 1 à 10.</h1>
-          <p>
-            Envoyez un courriel à{" "}
-            <a href="mailto:baptiste.lecocq@gmail.com">Baptiste Lecocq</a> et
-            présentez la carte de questions que vous souhaitez ajouter.
-          </p>
-
-          <p>
-            Encore merci pour l'investissement que vous souhaitez apporter à 1 à
-            10.
-          </p>
+          <iframe
+            title="Contribution"
+            src="https://docs.google.com/forms/d/e/1FAIpQLScoukKsCi3NgOIcAlX1RmPayqw9WBJD7wrlZB1gCL7vUlmqpA/viewform?embedded=true"
+            width="100%"
+            height="3243"
+            frameBorder="0"
+            marginHeight="0"
+            marginWidth="0"
+          >
+            Chargement…
+          </iframe>
         </div>
       </section>
     </Layout>
